d0e9794de1b3ecba6cfa4147e551afba350ea0ec
Tominator3Trillion <89489218+Tominator3Trillion@users.noreply.github.com>
2022-05-10 15:26:26 -0700

    All Complete including Challenge

---
+++ Assets/Challenge 5/Scripts/Timer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    public GameManagerX gameManagerX;
+
+    private TextMeshProUGUI timerText;
+    private float time;
+
+    public float maxTime = 60f;
+
+
+    void Start()
+    {
+        timerText = GetComponent<TextMeshProUGUI>();
+    }
+
+    void Update()
+    {
+        if(!gameManagerX.isGameActive)
+            return;
+
+        time += Time.deltaTime;
+        timerText.text = "Time: " + (maxTime- Mathf.Round(time));
+
+        if (time >= maxTime)
+        {
+            gameManagerX.GameOver();
+            time = 0;
+        }
+
+        
+
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> targets;
+
+    private float spawnRate = 1.5f;
+
+    private int score;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+
+    public Button restartButton;
+
+    public GameObject titleScreen;
+
+    public bool isGameActive;
+
+    public void StartGame() {
+        isGameActive = true;
+        titleScreen.SetActive(false);
+        StartCoroutine(SpawnTarget());
+        UpdateScore(0);
+    }
+
+    IEnumerator SpawnTarget() {
+        while (isGameActive) {
+            yield return new WaitForSeconds(spawnRate);
+            int randomIndex = Random.Range(0, targets.Count);
+            Instantiate(targets[randomIndex]);
+        }
+    }
+
+    public void UpdateScore(int scoreToAdd) {
+        score+=scoreToAdd;
+        scoreText.text = "Score: " + score;
+    }
+
+    public void GameOver() {
+        gameOverText.gameObject.SetActive(true);
+        restartButton.gameObject.SetActive(true);
+        isGameActive = false;
+    }
+
+    public void RestartGame() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void SelectDifficulty(int difficulty) {
+        if(difficulty == 1) {
+            spawnRate = 1.5f;
+        } else if(difficulty == 2) {
+            spawnRate = 1f;
+        } else if(difficulty == 3) {
+            spawnRate = 0.5f;
+        }
+    }
+
+}
